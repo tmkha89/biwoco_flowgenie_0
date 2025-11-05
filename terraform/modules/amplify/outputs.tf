121e5eba16b344edd6db3b0e9d7c27bbd34f4e49
tmkha89 <tmkha.1989@gmail.com>
2025-11-05 08:31:47 +0700

    Update outputs.tf

--- terraform/modules/amplify/outputs.tf
+++ terraform/modules/amplify/outputs.tf
@@ -10,10 +10,4 @@ output "app_arn" {
 
 output "app_url" {
   description = "Amplify App URL"
-  value       = "https://${aws_amplify_branch.main.branch_name}.${aws_amplify_app.main.default_domain}"
-}
-
-output "branch_name" {
-  description = "Amplify Branch Name"
-  value       = aws_amplify_branch.main.branch_name
 }
