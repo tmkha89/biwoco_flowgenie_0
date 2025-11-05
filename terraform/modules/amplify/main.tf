0304628bf67924fa1a2d7fce05ca33ef3dd7ebb5
tmkha89 <tmkha.1989@gmail.com>
2025-11-05 08:26:11 +0700

    Update main.tf

--- terraform/modules/amplify/main.tf
+++ terraform/modules/amplify/main.tf
@@ -1,43 +1,6 @@
 # Amplify App
 resource "aws_amplify_app" "main" {
   name         = "${var.stage}-flowgenie-frontend"
-  repository   = var.repository_url
-  access_token = "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"
-
-  build_spec = <<-EOT
-    version: 1
-    frontend:
-      phases:
-        preBuild:
-          commands:
-            - cd frontend
-            - npm ci
-        build:
-          commands:
-            - npm run build
-      artifacts:
-        baseDirectory: frontend/dist
-        files:
-          - '**/*'
-      cache:
-        paths:
-          - frontend/node_modules/**/*
-  EOT
-
-  environment_variables = merge(
-    {
-      AMPLIFY_DIFF_DEPLOY       = "false"
-      AMPLIFY_MONOREPO_APP_ROOT = "frontend"
-    },
-    var.environment_variables
-  )
-
-  tags = merge(
-    var.tags,
-    {
-      Name = "${var.stage}-flowgenie-frontend"
-    }
-  )
 }
 
 # Amplify Branch
